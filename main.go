a00c6028684c9f531ed6a06004811c65379f4a00
Abdulwahab Nour <abdelwahabnourr@gmail.com>
2021-04-14 07:14:46 +0200

    Create  a simple Contact view

--- main.go
+++ main.go
@@ -7,23 +7,40 @@ import (
 	"github.com/gorilla/mux"
 )
  
-var templ * template.Template
+var (
+  homeTempl * template.Template
+  contactTempl * template.Template
+)
+
 
  
 func home(w http.ResponseWriter, req *http.Request){
       w.Header().Set("Content-Type", "text/html")
-      if err := templ.Execute(w, nil); err != nil{
+      if err := homeTempl.Execute(w, nil); err != nil{
          panic(err)
-      }
+      } 
+}
+
+func contact (w http.ResponseWriter, r *http.Request){
+  w.Header().Set("Content-Type", "text/html")
+  if err := contactTempl.Execute(w, nil); err != nil{
+    panic( err)
+  }
 }
 func main() {
    var err error
-   templ, err = template.ParseFiles("views/home.gohtml")
+   homeTempl, err = template.ParseFiles("views/home.gohtml")
    if err != nil{
      panic(err)
    }
+
+  contactTempl, err = template.ParseFiles("views/contact.gohtml")
+  if err != nil{
+    panic(err)
+  }
+
  r := mux.NewRouter()
  r.HandleFunc("/", home)
- 
+ r.HandleFunc("/contact", contact)
  http.ListenAndServe(":3000", r)
 }
