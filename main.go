52817a621e40136fcec03e66e2f2a871a138d2ea
Abdulwahab Nour <abdelwahabnourr@gmail.com>
2022-02-09 00:58:41 +0200

    home view

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
- var homeTemplate = views.NewView("views/home.gohtml")
+var (
+	homeTemplate *views.View
+)
 
 func main() {
-	
+	homeTemplate = views.NewView("body","views/home.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	http.ListenAndServe(":8080", r)
@@ -21,12 +23,10 @@ func main() {
 func home(w http.ResponseWriter, r *http.Request) {
 	data := struct {
 		Name string
-     
 	}{
 		Name: "Ahmed",
-     
 	}
-	err := homeTemplate.Template.Execute(w, data)
+	err := homeTemplate.Template.ExecuteTemplate(w, homeTemplate.Layout,data)
 	if err != nil {
 		log.Fatalln(err)
 	}
--- views/view.go
+++ views/view.go
@@ -6,13 +6,14 @@ import "html/template"
 
 
 
-func NewView(files ...string) *View{
-     files = append(files, "views/layouts/footer.gohtml")
-     t:= template.Must(template.ParseFiles(files...))
-    return &View{Template:t}
+func NewView(layout string, files ...string) *View{
+        files = append(files,  "views/layouts/body.gohtml", "views/layouts/footer.gohtml")
+        t:= template.Must(template.ParseFiles(files...))
+        return &View{Template:t, Layout: layout}
 }
 
 type View struct{
     Template  *template.Template
+    Layout string
 }
 
